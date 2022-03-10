674a8b66084c4ddcae2ff10afa25554a6ebd5486
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-10 10:59:42 +0400

    Task2 Complete

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            
+            Console.WriteLine(deleter(ref sentence));
+        }
+        static string deleter(ref string sentence)
+        {
+            string sentence2 = sentence.Replace(" ","");
+            return sentence2;
+        }
+    }
+}
