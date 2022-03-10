c4f9aa4620609d084f4fad760f042af90da22e08
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-10 10:59:52 +0400

    Task3 Complete

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int given = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sqrt(given)); 
+        }
+        static int Sqrt(int given)
+        {
+            int result = 0;
+            while (given >= result * result) 
+            {
+                result++;
+            }
+            return result-1;
+        }
+    }
+}
