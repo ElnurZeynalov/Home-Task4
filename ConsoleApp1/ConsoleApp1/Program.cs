c5579bfd877c41b038cef3c73b787fbe5b1c0aec
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-10 10:59:29 +0400

    Task1 Complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,3,5};
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("=========");
+
+            foreach (var item in add(array,7))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static int[] add(int[] array , int n)
+        {
+            int[] array2 = new int[array.Length+1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array[i];
+            }
+            array2[array.Length] = n;
+            return array2;
+        }
+    }
+}
